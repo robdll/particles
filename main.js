1d8791b2de78e85ab38e22e2688075b271283a7d
unknown <dilillo.roberto@gmail.com>
2020-08-17 07:43:43 +0200

    adds Particle class

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const settings = {
-    radius: 150
+    radius: 150,
+    scanner: {
+        sTop: 0,
+        sLeft: 0,
+        width: 200,
+        height: 100
+    },
+    offset: 100
 }
 
 const canvas = document.getElementById("canvas1");
@@ -17,7 +24,35 @@ const mouse = {
 window.addEventListener('mousemove', event => {
     mouse.x = event.x;
     mouse.y = event.y;
-    console.log(mouse.x, mouse.y)
+    //console.log(mouse.x, mouse.y)
 });
 
+ctx.font = '30px Verdana';
+ctx.fillStyle = 'white';
+ctx.fillText('Test Text', 0, 30);
+//ctx.strokeStyle = 'white';
+// ctx.strokeRect(sTop, sLeft, width, height);
+const {sTop, sLeft, width, height} = settings.scanner;
+const data = ctx.getImageData(sTop, sLeft, width, height);
+
+class Particle {
+    constructor(x, y, size = 3) {
+        this.x = x + settings.offset;
+        this.y = y;
+        this.size = size;
+        this.baseX = this.x;
+        this.baseY = this.y;
+        this.density = (Math.random() * 30) +1;
+    }
+
+    draw() {
+        ctx.fillStyle = 'white';
+        ctx.beginPath();
+        ctx.ard(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+        console.log('drowing')
+    }
+
+}
 
